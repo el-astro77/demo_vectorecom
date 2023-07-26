574b4c5b3df02881a2c302de55ea7c22b69b6ae8
SWARAJ BANGAR <swarajbangar77@gmail.com>
2023-07-26 15:35:31 +0530

    initial commit

---
+++ src/screens/Desktop/Desktop.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap">
+          <div className="rectangle" />
+          <div className="text-wrapper">SB</div>
+        </div>
+        <div className="text-wrapper-2">Vectors</div>
+        <div className="text-wrapper-3">Photos</div>
+        <div className="text-wrapper-4">PSD</div>
+        <div className="text-wrapper-5">Videos</div>
+        <div className="text-wrapper-6">Latest</div>
+        <div className="text-wrapper-7">Popular</div>
+        <div className="text-wrapper-8">Premium</div>
+        <div className="text-wrapper-9">Free</div>
+        <div className="text-wrapper-10">|</div>
+        <div className="text-wrapper-11">More</div>
+        <div className="overlap-group">
+          <div className="text-wrapper-12">+ Submit</div>
+          <div className="rectangle-2" />
+        </div>
+        <img className="gmail" alt="Gmail" src="/img/gmail-1.png" />
+        <img className="facebook" alt="Facebook" src="/img/facebook-1.png" />
+        <h1 className="h-1">Popular Pro Stock Vectors</h1>
+        <p className="p">High quality Vectors with worry-free licensing for personal and commercial use.</p>
+        <div className="overlap-2">
+          <div className="div-wrapper">
+            <div className="text-wrapper-13">Vectors ⌄</div>
+          </div>
+          <div className="text-wrapper-14">Search Vectors...</div>
+          <img className="search" alt="Search" src="/img/icons8-search-64-1.png" />
+        </div>
+        <img className="slider" alt="Slider" src="/img/icons8-slider-50-1-1.png" />
+        <img className="bookmark" alt="Bookmark" src="/img/icons8-bookmark-48-1.png" />
+        <div className="overlap-group-2">
+          <div className="rectangle-3" />
+          <div className="text-wrapper-15">Download</div>
+          <div className="div-2" />
+          <img className="element-generated" alt="Element generated" src="/img/002-generated-photoroom-1.png" />
+          <div className="text-wrapper-16">D</div>
+          <div className="text-wrapper-17">og illustration</div>
+          <img className="download" alt="Download" src="/img/icons8-download-26-1.png" />
+        </div>
+        <div className="overlap-3">
+          <div className="div-2">
+            <img className="dl" alt="Dl" src="/img/dl-1.png" />
+          </div>
+          <div className="text-wrapper-18">A</div>
+          <div className="text-wrapper-19">stronaut riding</div>
+          <div className="text-wrapper-20">Download</div>
+          <img className="icons-download" alt="Download" src="/img/icons8-download-26-2.png" />
+        </div>
+        <div className="overlap-4">
+          <div className="div-2">
+            <img
+              className="dl-beatsnoop-com"
+              alt="Dl beatsnoop com"
+              src="/img/dl-beatsnoop-com-high-1aa91e4fd1e911c398-photoroom-1.png"
+            />
+          </div>
+          <div className="overlap-5">
+            <div className="text-wrapper-21">R</div>
+            <div className="text-wrapper-22">ockstar guitarist</div>
+          </div>
+          <div className="text-wrapper-23">Download</div>
+          <img className="img" alt="Download" src="/img/icons8-download-26-3.png" />
+        </div>
+        <div className="text-wrapper-24">discover all collections</div>
+        <div className="overlap-6">
+          <div className="text-wrapper-25">Next page</div>
+          <img className="arrow" alt="Arrow" src="/img/icons8-arrow-64-1-1.png" />
+        </div>
+        <div className="text-wrapper-26">Page</div>
+        <div className="text-wrapper-27">of 120</div>
+        <div className="overlap-7">
+          <div className="text-wrapper-28">&lt;</div>
+        </div>
+        <div className="overlap-8">
+          <div className="text-wrapper-28">1</div>
+        </div>
+        <div className="overlap-9">
+          <div className="text-wrapper-28">&gt;</div>
+        </div>
+      </div>
+    </div>
+  );
+};
